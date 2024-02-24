17cc21665b15437d36074fe097e95e9ddcedd7bd
Gabriel Lima <gabriel.lbs002@gmail.com>
2024-02-24 01:11:29 -0300

    feat: adjustments + language selection

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
-import argparse
 from scrapy.crawler import CrawlerProcess
 
 from sage.spiders import QuotesSpider
 
-def start_crawler(slug: str):
-  process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'quotes.json',
-  })
 
-  process.crawl(QuotesSpider, slug=slug)
-  
-  process.start()
+def start_crawler(slug: str, languageCode: str):
+    process = CrawlerProcess(
+        settings={
+            "FEED_FORMAT": "json",
+            "FEED_URI": "quotes.json",
+        }
+    )
+
+    process.crawl(QuotesSpider, slug=slug, lang=languageCode)
+
+    process.start()
+
 
 def main():
-  print('Welcome to Sage - Get all popular quotes from a book.')
-  print('\nFirst, search "your book" + quotes on Google, and check for the slug on it\'s Goodreads quotes page.')
-  print('The URL must look like "goodreads.com/work/quotes/<slug>".')
-  print('\n\nQuotes will be exported in ./quotes.json')
+    print("Welcome to Sage - Get all popular quotes from a book!")
+    print(
+        '\nSearch "<your book> quotes" on your search engine, and check for the slug on it\'s Goodreads quotes page URL.'
+    )
+    print('The URL must look like "goodreads.com/work/quotes/<slug>".')
+    print("\n\nQuotes will be exported to ./quotes.json")
 
-  slug = input("\nPlease enter the slug of the book: ")
+    slug = input("\nPlease enter the slug of the book: ")
 
-  while not slug:
-    slug = input("Please enter the slug of the book: ")
-  
-  start_crawler(slug)
+    while not slug:
+        slug = input("Please enter the slug of the book: ")
 
-if __name__ == '__main__':
-  main()
+    languageCode = input(
+        '\nPlease enter the desired language code (ex: "en" for english): '
+    )
+
+    while not languageCode:
+        languageCode = input(
+            'Please enter the desired language code (ex: "en" for english): '
+        )
+
+    start_crawler(slug, languageCode)
+
+
+if __name__ == "__main__":
+    main()
--- sage/spiders/__init__.py
+++ sage/spiders/__init__.py
@@ -1,13 +1,15 @@
 from langdetect import detect
 import scrapy
 
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    def __init__(self, slug=None, *args, **kwargs):
+    def __init__(self, slug=None, lang=None, *args, **kwargs):
         super(QuotesSpider, self).__init__(*args, **kwargs)
-        self.start_urls = ['https://www.goodreads.com/work/quotes/' + slug]
+        self.start_urls = ["https://www.goodreads.com/work/quotes/" + slug]
         self.slug = slug
+        self.lang = lang
         self.quotes_seen = set()
 
     def parse(self, response):
@@ -16,18 +18,20 @@ class QuotesSpider(scrapy.Spider):
         for quote in quote_divs:
             quote_text = quote.xpath('.//div[@class="quoteText"]/text()').get().strip()
 
-            author = quote.xpath('.//span[@class="authorOrTitle"]/text()').get().replace(",", "").strip()
+            author = (
+                quote.xpath('.//span[@class="authorOrTitle"]/text()')
+                .get()
+                .replace(",", "")
+                .strip()
+            )
 
-            if detect(quote_text) == 'en':
+            if detect(quote_text) == self.lang:
                 if quote_text not in self.quotes_seen:
                     self.quotes_seen.add(quote_text)
-                    yield {
-                        'quote': quote_text,
-                        'author': author
-                    }
-        
+                    yield {"quote": quote_text, "author": author}
+
         next_page = response.xpath('//a[@class="next_page"]/@href').get()
-        
+
         if next_page:
             next_page_url = response.urljoin(next_page)
 
@@ -36,4 +40,3 @@ class QuotesSpider(scrapy.Spider):
             self.logger.info(f"Scraping page {page_number}!")
 
             yield scrapy.Request(next_page_url, callback=self.parse)
-		
